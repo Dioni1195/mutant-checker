d5dd6c884710d069506927f1e2c607a0e8a012ce
DionisioArango-SB <dionisio.arango95@gmail.com>
2021-08-31 21:53:48 -0500

    Corregir algunos code smells

--- src/main/java/com/mutant/checker/controller/customConstraints/FirstOrder.java
+++
@@ -1,4 +0,0 @@
-package com.mutant.checker.controller.customConstraints;
-
-public interface FirstOrder {
-}
---
+++ src/main/java/com/mutant/checker/controller/custom_constraints/SecondOrder.java
@@ -0,0 +1,4 @@
+package com.mutant.checker.controller.custom_constraints;
+
+public interface SecondOrder {
+}
