5425fd16e7d9834777d25354967e8b3a7a383a07
DionisioArango-SB <dionisio.arango95@gmail.com>
2021-08-31 20:36:52 -0500

    Eliminar constraint de empty

--- src/main/java/com/mutant/checker/service/dto/CheckDnaRequestDTO.java
+++ src/main/java/com/mutant/checker/service/dto/CheckDnaRequestDTO.java
@@ -9,7 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.GroupSequence;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
